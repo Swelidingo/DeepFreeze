5bfaadb389b17456e559003f10113531d49cfe09
Swelidingo <104819606+Swelidingo@users.noreply.github.com>
2022-11-13 00:05:23 +0100

    Update Vallon5.bat

--- Vallon5.bat
+++ Vallon5.bat
@@ -1,2 +1,2 @@
-xcopy "\\mdtsrvr\applications\Vallon 5" "c:\program files (x86)" /E /H
+xcopy "\\mdtsrvr\applications\Vallon 5" "c:\program files (x86)\Vallon 5" /E /H /C /I
 xcopy "\\mdtsrvr\applications\Vallon 5\vallon5.lnk" "c:\users\public\desktop"
