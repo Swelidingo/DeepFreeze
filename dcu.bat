b47a9d6a9147fc034f0aff8427e415ed8d7e42e4
Swelidingo <104819606+Swelidingo@users.noreply.github.com>
2022-11-23 22:14:30 +0100

    Update dcu.bat

--- dcu.bat
+++ dcu.bat
@@ -1,1 +1,1 @@
-c:\dcu-cli.exe" -Parameters '/applyupdates -reboot=enable -encryptedPasswordFile=\\mdtsrvr\applications\dcu\EncryptedPassword.txt -encryptionKey="Mittnamn1!"'
+c:\dcu-cli.exe" /applyupdates -reboot=enable -encryptedPasswordFile="\\mdtsrvr\applications\dcu\EncryptedPassword.txt" -encryptionKey="Mittnamn1!"
